1e6bc43ec729a310c1bb43e701bf5d82bf3be771
yaravi <yaravi@gmail.com>
2021-09-27 13:42:21 -0500

    Update search.js

--- DataScience/searchAlgorithms/search.js
+++ DataScience/searchAlgorithms/search.js
@@ -51,3 +51,5 @@ function busquedaBinaria(arreglo, datoBuscado) {
 const arreglo3 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
 
 console.log(busquedaLineal(arreglo3, 18));
+
+//------------------------------
